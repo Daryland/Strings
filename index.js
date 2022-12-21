5310e9f0f177774ecbbd11389d9f0296246c5f37
foesublime <rylandcs@gmail.com>
2022-12-21 16:19:33 -0700

    Strings, type, length, etc

---
+++ index.js
@@ -0,0 +1,18 @@
+// JavaScript Strings
+
+var make = 'Toyota';
+var model = "Prius";
+var year = 2021;
+
+//  String Concatenation 
+// Ability to join or add strings together
+
+var myRide = make + '' + model;
+
+var myBraggingRights = 'I drive a ' + year + ' ' + myRide;
+// Spaces count as character counts
+
+console.log(myBraggingRights);
+console.log( typeof myBraggingRights);
+
+console.log(myRide.length);
